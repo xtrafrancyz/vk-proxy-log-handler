8cdadc0b2bee62680c0b907b93d0a4816275771c
xtrafrancyz <xtrafrancyz@gmail.com>
2017-08-22 22:20:29 +0300

    Initial commit

---
+++ server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/vharitonsky/iniflags"
+)
+
+type StatEntry struct {
+	Online   int `json:"online"`
+	Traffic  int `json:"traffic"`
+	Requests struct {
+		Api    int `json:"api"`
+		Assets int `json:"assets"`
+		Total  int `json:"total"`
+	} `json:"requests"`
+}
+
+var data = make(map[int64]*StatEntry)
+var cIp = make(chan string, 20)
+var uniquesMap = make(map[string]int64)
+
+func handleLog(entry LogEntry) {
+	key := entry.time.Unix() - int64(entry.time.Second())
+
+	stats, ok := data[key]
+	if !ok {
+		stats = &StatEntry{}
+		for k := range data {
+			if k-key > 2*24*60*60 {
+				delete(data, k)
+			}
+		}
+		data[key] = stats
+	}
+	stats.Requests.Total++
+	stats.Traffic += entry.length
+	if strings.HasPrefix(entry.path, "/_") {
+		stats.Requests.Assets++
+	} else {
+		stats.Requests.Api++
+	}
+
+	cIp <- entry.ip
+	stats.Online = len(uniquesMap)
+}
+
+func handleWeb(w http.ResponseWriter, r *http.Request) {
+	encoded, _ := json.Marshal(data)
+	fmt.Fprintf(w, string(encoded))
+}
+
+func main() {
+	webHost := flag.String("web-host", "0.0.0.0:13554", "address to bind web")
+	syslogHost := flag.String("syslog-host", "0.0.0.0:7423", "address to bind syslog")
+
+	iniflags.Parse()
+
+	ticker := time.NewTicker(5 * time.Second)
+	go func() {
+		for {
+			select {
+			case ip := <-cIp:
+				uniquesMap[ip] = time.Now().Unix()
+			case <-ticker.C:
+				curr := time.Now().Unix()
+				for ip, lastAccess := range uniquesMap {
+					if curr-lastAccess > 3*60 {
+						delete(uniquesMap, ip)
+					}
+				}
+			}
+		}
+	}()
+
+	http.HandleFunc("/", handleWeb)
+	go http.ListenAndServe(*webHost, nil)
+	startSyslog(*syslogHost).Wait()
+}
---
+++ syslog.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+	"time"
+
+	"gopkg.in/mcuadros/go-syslog.v2"
+)
+
+type LogEntry struct {
+	time    time.Time
+	message string
+	length  int
+	method  string
+	path    string
+	ip      string
+}
+
+var logRegex = regexp.MustCompile(`([^ ]+) [^"]+"([A-Z]+) ([^ ]+) [^"]+" [0-9]+ ([0-9]+)`)
+
+func startSyslog(host string) *syslog.Server {
+	channel := make(syslog.LogPartsChannel)
+	handler := syslog.NewChannelHandler(channel)
+
+	server := syslog.NewServer()
+	server.SetFormat(syslog.RFC3164)
+	server.SetHandler(handler)
+	server.ListenUDP(host)
+	server.Boot()
+
+	go func(channel syslog.LogPartsChannel) {
+		for record := range channel {
+			entry := LogEntry{}
+			entry.message = record["content"].(string)
+			entry.time = record["timestamp"].(time.Time)
+			match := logRegex.FindStringSubmatch(entry.message)
+			entry.ip = match[1]
+			entry.method = match[2]
+			entry.path = match[3]
+			entry.length, _ = strconv.Atoi(match[4])
+			handleLog(entry)
+		}
+	}(channel)
+
+	return server
+}
